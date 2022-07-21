bc3c5ed811dec6cdb9ed7adb087f84830053fc45
Squad Ready <squadready@Mac-mini-Squad.local>
2022-07-21 11:05:06 +0600

    vyasan

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,11 @@
 import React from 'react';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import NavTabs from './NavTabs';
-import Work from './Work';
-import Education from './Education';
-import Hobbys from './Hobbys';
 
 function App() {
-  const [position, setPosition] = React.useState('work')
-  const onSelect = (eventKey: string | null) => {
-      setPosition(eventKey ? eventKey : '')
-  }
-
-  const Content:React.FC = () => {
-    switch(position) {
-      case 'work': return <Work/>;
-      case 'edu': return <Education/>;
-      case 'hobby': return <Hobbys/>;
-      default: return null
-    }
-  }
 
   return (
     <div className="App">
-      <div className='Photo'>
-        <img className='Img' src={process.env.PUBLIC_URL + '/photo.jpg'} />
-        <div className='LastName'>Bizin</div>
-        <div className='Name'>Semyon</div> 
-        <div className='Age'>22 years old</div> 
-      </div>
-      <div className='Info'>
-        <NavTabs onSelected={onSelect}/>
-        <div className='Content'>
-          <Content/>
-        </div>
-      </div>
+
     </div>
   );
 }
